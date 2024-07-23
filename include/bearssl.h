f59ab110d3ed6e76f93b1d723c868f9ef12e94cd
archibate <1931127624@qq.com>
2024-07-23 13:58:59 +0800

    add bearssl

---
+++ include/bearssl.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#ifndef __clang__
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+#pragma GCC diagnostic ignored "-Wold-style-cast"
+#pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+
+#include <bearssl/bearssl.hpp>
+
+#pragma GCC diagnostic pop
